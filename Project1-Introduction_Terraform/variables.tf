00066c847aa77135e76538c8a7af186ea374d042
Lucas Cabral Mendes Corrêa <lucascabralmendes.correa@gmail.com>
2022-01-26 11:30:05 -0300

    Add files via upload

---
+++ Project1-Introduction_Terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "locat" {
+    default = "eastus"
+}
+
+variable "rg" {
+    default = "rg-terraform"
+}
+
+variable "vnet" {
+    default = "vnet-terraform"
+}
+
+variable "vm" {
+    default = "vm-terraform"
+}
